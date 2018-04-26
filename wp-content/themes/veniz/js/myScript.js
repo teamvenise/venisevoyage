54d84e56b5b9cd01e8763d7e6df1c2d61157a274
Unknown <zo.ravalitera@outlook.com>
2018-04-26 09:30:52 +0300

    Corrections pour page contact

--- wp-content/themes/veniz/js/myScript.js
+++ wp-content/themes/veniz/js/myScript.js
@@ -3,4 +3,7 @@ jQuery(document).ready (function(){
     datepickerHandler.datepicker({
         dateFormat: "dd-mm-yy"
     });
+
+    jQuery(".w50").parent().addClass("width50");
+    jQuery(".wpcf7-submit").addClass("round-btn");
 });
